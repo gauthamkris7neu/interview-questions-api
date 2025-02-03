6a4851660c54ed75dd41b0a44887fe26270afc76
gauthamkris7neu <venkatakrishnapras.g@northeastern.edu>
2025-02-03 18:28:48 -0500

    Added Database Integration

---
+++ database.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
